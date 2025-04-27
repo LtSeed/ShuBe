c30958e85174c8336b85c72d028423fe1c3eb621
LtSeed <Liuseed@outlook.com>
2025-04-27 16:11:36 +0800

    init

---
+++ src/main/java/org/example/shubackend/ShubackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.shubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShubackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShubackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/shubackend/ShubackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShubackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
